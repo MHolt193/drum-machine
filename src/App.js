2701bf78d4851be395236c4d45be73cf76c901ae
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2021-08-01 12:48:58 -0600

    Added keyPressHandler

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import NavBar from "./Components/NavBar";
 import KeyPad from "./Components/KeyPad";
 import Display from "./Components/Display";
+import Info from "./Components/Info";
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,12 +11,15 @@ class App extends React.Component {
       volume: 0.5,
       power: "false",
       sound: "Loading...",
-    };
+      modalShow: false,
+    };//bind all handlers
     this.modeHandler = this.modeHandler.bind(this);
     this.volumeHandler = this.volumeHandler.bind(this);
     this.powerHandler = this.powerHandler.bind(this);
     this.clickHandler = this.clickHandler.bind(this);
-  }
+    this.keyPressHandler = this.keyPressHandler.bind(this);
+    this.setModalShow = this.setModalShow.bind(this);
+  }//define handlers
   modeHandler = (event) => {
     this.setState({ mode: event.target.innerText });
   };
@@ -27,9 +31,34 @@ class App extends React.Component {
       ? this.setState({ power: "true" })
       : this.setState({ power: "false" });
   };
-  clickHandler(event) {
-    document.getElementById(event.target.innerText).play();
+  clickHandler = (event) => {
+    let audio = document.getElementById(event.target.innerText);
+    audio.currentTime = 0;
+    audio.volume = this.state.volume;
+    audio.play();
     this.setState({ sound: event.target.id });
+  };
+  keyPressHandler = (event) => {
+    if ("QWEASDZXC".includes(event.key.toUpperCase())) {
+      let audio = document.getElementById(event.key.toUpperCase());
+      audio.currentTime = 0;
+      audio.volume = this.state.volume;
+      audio.play();
+      this.setState({ sound: audio.parentNode.id });
+    }
+  };
+  setModalShow = () => {
+    this.state.modalShow === false
+      ? this.setState({ modalShow: true })
+      : this.setState({ modalShow: false });
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.keyPressHandler);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.keyPressHandler);
   }
   render() {
     return (
@@ -40,13 +69,19 @@ class App extends React.Component {
           volume={this.state.volume}
           powerHandler={this.powerHandler}
           power={this.state.power}
+          info={this.setModalShow}
         />
         <Display value={this.state.sound} power={this.state.power} />
         <KeyPad
           volume={this.state.volume}
+          keyPressHandler={this.keyPressHandler}
           clickHandler={this.clickHandler}
           mode={this.state.mode}
           power={this.state.power}
+        />
+        <Info
+          show={this.state.modalShow}
+          onHide={this.setModalShow}
         />
       </div>
     );
